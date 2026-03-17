2e1fa1815b486d807603aba2d44941c734e9645d
Enko-Aniki <enzodfsilva@gmail.com>
2026-03-17 10:24:04 -0300

    initial commit

---
+++ QuizReact/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+import Wellcome from "./components/Wellcome"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Quiz de Ingles</h1>
+      <Wellcome />
+      
+
+    </div>
+  )
+
+}
+
+export default App
---
+++ QuizReact/src/components/Wellcome.jsx
@@ -0,0 +1,15 @@
+import Quiz from "../img/quiz.svg";
+import './Wellcome.css'
+const Wellcome = () => {
+  return (
+    <div id="Quiz Ingles">
+        <h2> Wellcome to quiz </h2>
+        <p>Click on button to start</p>
+        <img src={Quiz} alt="Quiz Begginig" />
+        <button>Start</button>
+
+    </div>
+  )
+}
+
+export default Wellcome
